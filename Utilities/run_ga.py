af2b194d9e7f96235c609887f9fa02d23201eb98
j-alex-vindel <jose.vindel-garduno@strath.ac.uk>
2023-02-02 17:42:37 +0000

    Utilities folder and run_ga template created

---
+++ Utilities/run_ga.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+sys.path.append(os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'Objects')))
+
+from Met_Net import Metabolic_Network
+from Individual import Individual
+
+## To continue workig
