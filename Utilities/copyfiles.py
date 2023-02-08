c5dd7a6a5f5754ddea92ca0c7253e24380d42f16
j-alex-vindel <jose.vindel-garduno@strath.ac.uk>
2023-02-08 11:01:48 +0000

    copyfiles modified

--- Utilities/copyfiles.py
+++ Utilities/copyfiles.py
@@ -3,45 +3,35 @@ import os
 import time
 import shutil
 
-
-# Modify so it can take the file's new name
-
-def copy_file_chname(cfile:str=None,bacteria:list=None,nfiles:int=None,t:int=2):
-
+def file_copier(folder:str=None,o_file:str=None,d_folder:str=None,c_file:str=None):
     path = os.getcwd()
     parent_directory = os.path.abspath(os.path.join(path,os.pardir))
-    print(f"Parent Dir: <{parent_directory}>")
-    sfile = f"Utilities/{cfile}"
-    source = os.path.abspath(os.path.join(parent_directory,sfile))
-
-    folder = f"B_{bacteria}"
-    dest_folder = os.path.join(parent_directory,folder)
-    cursizedir = len(os.listdir(dest_folder))
-    print(f"Destiny Dir: {dest_folder}")
-
-    ks = [i for i in range(nfiles)]
     
-    print(f'From {source} ->')
+    print(f">> Information about Source <<")
+    folder = input(">> Enter name of the folder: \n -> ")
+    original_folder = os.path.join(parent_directory,folder)
+    print(f"Files in the source folder : \n {os.listdir(original_folder)}")
+    
+    
+    
+    o_file = input(">> Enter the name of the file: \n -> ")
+    source = f"{folder}/{o_file}"
+    source_dir = os.path.abspath(os.path.join(parent_directory,source))
 
-    for k in ks:
-        if k == 0:
-            file = f"{cfile[:7]}{bacteria}.py"
-        else:
-            file = f"{cfile[:7]}{bacteria}_{k}.py"
-        
-        dest_file = os.path.join(dest_folder,file)
-        print(dest_file)
-        shutil.copyfile(source,dest_file)
 
-    finsizedir = len(os.listdir(dest_folder))
 
-    print(f"# {finsizedir-cursizedir} File(s) Added!!")
+    print(f">> Information about Destination <<")
+    d_folder = input(">> Enter name of the folder: \n -> ") 
+    d_dir = os.path.join(parent_directory,d_folder)
 
-    time.sleep(t)
+    print(f"Files in the destinarion folder : \n {os.listdir(d_dir)}")
 
-if __name__ == "__main__":
+    c_file = input(">> Enter new name of file: \n -> ")
+    newfile = os.path.join(d_dir,c_file)
 
-    file_to_copy = sys.argv[1]
-    bacteria = sys.argv[2]
-    n_copies = int(sys.argv[3])
-    copy_file_chname(cfile=file_to_copy,bacteria=bacteria,nfiles=n_copies)
+    shutil.copyfile(source_dir,newfile)
+
+    print("New file copied!!")
+
+if __name__ == '__main__':
+    file_copier()
