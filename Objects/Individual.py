03e3eda8ee357e862133039cc267d932efc0d685
j-alex-vindel <jose.vindel-garduno@strath.ac.uk>
2023-02-10 17:07:49 +0000

    Individual module improved

--- Objects/Individual.py
+++ Objects/Individual.py
@@ -6,23 +6,32 @@ from dataclasses_json import dataclass_json
 
 Genome = List[int]
 
-@dataclass
-class Individual:
-    Gene: Genome = None
-    Cost: float = None
-    Result:namedtuple =  None
-    Bio: float = None
-    Chem: float = None
-    # TrueBio: float = None
-    # TrueChem: float = None
-    # Strat: List[str] = None
 
-Population = List[Individual]
-
-@dataclass_json
-@dataclass
-class Out_Result:
-    Pop: Population = None
-    BestSol: Any = None
-    BestCost: float = None 
+class Individual(object):
     
+    def __init__(self):
+
+        self.Gene: Genome = None
+        self.Cost: float = None
+        self.Bio: float = None
+        self.Chem: float = None
+        self.rank = None
+        self.crowding_distance = None
+        self.dominated_solutions = None
+        self.features = None
+        self.objectives = None
+
+    def __eq__(self, other):
+        if isinstance(self,other.__class__):
+            return self.features == other.features
+        return False
+
+    def dominates(self, other_individual):
+        and_condition = True
+        or_condition = False
+        
+        for first, second in zip(self.objectives, other_individual.objectives):
+            and_condition = and_condition and first <= second
+            or_condition = or_condition or first < second
+        
+        return (and_condition and or_condition)
